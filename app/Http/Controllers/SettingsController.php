e7fbb5103f6a708ff3dc27e5669a3f63a3a74d2c
MigsGit <miguellgsp12345@gmail.com>
2025-01-20 19:09:51 +0800

    DataTable UserMaster Module

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -15,6 +15,16 @@ class SettingsController extends Controller
             $user = User::whereNull('deleted_at')->get();
 
             return DataTables::of($user)
+            ->addColumn('get_action',function($row){
+                // return $row->id;
+                return $btn = '<button data-id = "'.$row->id.'"  class="btn btn-outline-info btn-sm" data-toggle="modal" id="btnEdocs" type="button" title="Edit"><i class="fas fa-edit"></i></button>';
+                // return $btn = '<button data-id = "'.$row->id.'" id="editResProcedure" type="button" class="btn btn-info btn-sm" title="Edit"></i>Edit</button>';
+            })
+            ->addColumn('get_status',function($row){
+                return $result = "Active";
+                // return $btn = '<button data-id = "'.$row->id.'" id="editResProcedure" type="button" class="btn btn-info btn-sm" title="Edit"></i>Edit</button>';
+            })
+            ->rawColumns(['get_action','get_status'])
             ->make(true);
             // return response()->json(['is_success' => 'true']);
         } catch (Exception $e) {
--- resources/js/pages/UserMaster.vue
+++ resources/js/pages/UserMaster.vue
@@ -2,15 +2,11 @@
 <div class="container-fluid">
 
 <!-- Page Heading -->
-<h1 class="h3 mb-2 text-gray-800">Tables</h1>
-<p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
-    For more information about DataTables, please visit the <a target="_blank"
-        href="https://datatables.net">official DataTables documentation</a>.</p>
-
+<h1 class="h3 mb-2 text-gray-800">User Master Table</h1>
 <!-- DataTales Example -->
 <div class="card shadow mb-4">
     <div class="card-header py-3">
-        <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+        <h6 class="m-0 font-weight-bold text-primary">User Master</h6>
     </div>
     <div class="card-body">
         <div class="table-responsive">
@@ -37,11 +33,6 @@
                         <th>Name</th>
                         <th>Email</th>
                         <th>Roles</th>
-                        <!-- <th class="w-25">Category</th>
-                        <th class="w-25">Document Name</th>
-                        <th class="w-50">Document</th>
-                        <th>Attachment</th>
-                        <th>Report Approvers</th> -->
                     </tr>
                 </thead>
             </DataTable>
@@ -77,7 +68,7 @@
                 }
             },
         },
-        { data: 'status'},
+        { data: 'get_status'},
         { data: 'name'},
         { data: 'email'},
         { data: 'roles'},
